4f0b191c92899dc0e6b52cdd0be37c96013fb884
Catalin Dinuta <43060213+dinuta@users.noreply.github.com>
2019-09-19 17:50:42 +0300

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+docker build . -t dinutac/estuary-viewer:"${TRAVIS_TAG}"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push dinutac/estuary-viewer:"${TRAVIS_TAG}"
