79219b0cc6c65af06edba2242c6964dafd22d7c1
cdinuta <constantin.dinuta@gmail.com>
2019-09-21 21:59:56 +0300

    reverted to the last config. deployments on latest are working

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 docker build . -t dinutac/estuary-viewer:"${TRAVIS_TAG}"
-docker build . -t dinutac/estuary-viewer:latest
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push dinutac/estuary-viewer:"${TRAVIS_TAG}"
-docker push dinutac/estuary-viewer:latest
