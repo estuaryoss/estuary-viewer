0aefc9d3e32e994cd6c074346226cc6b86f9ce08
cdinuta <cdinuta@axway.com>
2020-09-13 19:12:22 +0300

    updated badges for the new estuaryoss org

--- docker_push_master.sh
+++ docker_push_master.sh
@@ -3,5 +3,5 @@
 echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
 # build and push
-docker build . -t dinutac/estuary-viewer:latest
-docker push dinutac/estuary-viewer:latest
+docker build . -t estuaryoss/viewer:latest
+docker push estuaryoss/viewer:latest
