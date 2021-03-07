bc78560b989f9e188c99f474d7200e6c3076e475
Catalin Dinuta <constantin.dinuta@gmail.com>
2021-03-07 19:45:39 +0200

    changed image names

--- docker_push_tag.sh
+++ docker_push_tag.sh
@@ -3,5 +3,5 @@
 echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
 # build and push
-docker build . -t dinutac/estuary-viewer:"${TRAVIS_TAG}"
-docker push dinutac/estuary-viewer:"${TRAVIS_TAG}"
+docker build . -t estuaryoss/viewer:"${TRAVIS_TAG}"
+docker push estuaryoss/viewer:"${TRAVIS_TAG}"
